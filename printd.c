91d40eb09db79873ff1a59d12b6b5e963ed0c25f
JOHNSON.GM <gichukijohnson93@gmail.com>
2022-07-11 16:15:56 +0300

    add printd.c

---
+++ printd.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_d - print out decimals no. (base 10)
+ *
+ * @d: parameter of type va_list, what to print
+ *
+ * return: 0 (success)
+ */
+int print_d(va_list d)
+{
+        int d = va_arg(va_list d, int);
+
+	_putchar(d);
+	va_end(va_list d);
+	return (0);
+}
+
