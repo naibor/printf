1b660aaa7894b498d760ebfe95e64ac2730731a6
naibor <liznaibor.en@gmail.com>
2022-07-11 12:41:52 +0300

    add function get_format

--- _printf.c
+++ _printf.c
@@ -1,4 +1,38 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * get_format - Function pointer
+ *
+ * Description: get a format specifier
+ *
+ * @format: constant pointer parameter of type char, format specifier
+ *
+ * Return: returns pointer to function
+ */
+int (*get_format(const char *format))(va_list)
+{
+	unsigned int index;
+	print_type specifier[] = {
+		
+	}
+
+	for (index = 0; specifier[index].type != NULL; index++)
+	{
+		if (*(specifier[index].type) == *format)
+			break;
+	}
+	return (specifier[index].f);
+}
+
+
+
+
+
+
+
+
 /**
  * _printf -  Function
  * Description: produces output according to a format.
@@ -9,5 +43,20 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list a_list;
+
+	if (!format || format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(a_list, format);
+	
+	/*va_list a_list;*/
+	printf("THIS IS FORMAT:%s\n", format);
+	printf("INDXES OF FORMAT: %c\n", format[0]);
+	/*va_start(format, a_list);*/
+
+	
         return (0);
 }
