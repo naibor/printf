f84f18b2430024ae9971f5ae09feb2fe6d782701
JOHNSON.GM <gichukijohnson93@gmail.com>
2022-07-11 14:02:06 +0300

    fix betty errors

--- prints.c
+++ prints.c
@@ -2,9 +2,11 @@
 /*
  * print_s - prints string
  *
- * @ch parameter used
+ * description: prints a string
  *
- * return (0) success
+ * @ch: parameter used
+ *
+ * Return: (0) success
  */
 
 int print_s(va_list ch)
@@ -12,14 +14,14 @@ int print_s(va_list ch)
 	 char str = (char)va_arg(va_list ch, char *);
 	 unsigned int index;
 
-	 if (str == NULL)
+	if (str == NULL)
 		str = "(null)";
-	 for (index = 0; str[index]; index++)
-	 {
+	for (index = 0; str[index]; index++)
+	{
 		_putchar(str[index]);
-	 }
+	}
 
-        va_end(va_list ch);
-        return (index);
+	va_end(va_list ch);
+	return (index);
 }
 
