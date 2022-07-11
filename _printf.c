73059d70de17c3f5e07dbe605de9b934b1186a51
naibor <liznaibor.en@gmail.com>
2022-07-11 10:32:08 +0300

    added function description and definition

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _printf -  Function
+ * Description: produces output according to a format.
+ *
+ * @format: pointer parameter 
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+        return (0);
+}
