856dad10c0b52d39517eb86d6e61dbdee835d4a2
naibor <liznaibor.en@gmail.com>
2022-07-13 00:04:54 +0300

    update print integer function to use helper function print number

--- printi.c
+++ printi.c
@@ -12,24 +12,10 @@
 int print_i(va_list i)
 {
 	int count = 0;
-	int x = 1;
 	int num = va_arg(i, int);
 	
-	if (num < 0)
-	{
-		count+= _putchar('-');
-		num = num * -1;
-	}
-	for (; num / x > 9;)
-	{
-		x *= 10;
-	}
-	for (; x != 0;)
-	{
-		count+= _putchar('0' + num/x);
-		num %= x;
-		x /= 10;
-	}
+	count++;
+	print_number(num);
 
 	return (count);
 }
