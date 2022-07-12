15ae753ed2f5b792aa88f33a7970f89b2011ead2
naibor <liznaibor.en@gmail.com>
2022-07-13 00:05:29 +0300

    include print number prototype to header

--- main.h
+++ main.h
@@ -20,6 +20,7 @@ int _printf(const char *format, ...);
 int (*get_format(const char *format))(va_list);
 int print_c(va_list ch);
 int print_s(va_list ch);
+void print_number(int n);
 int print_d(va_list d);
 int print_i(va_list i);
 #endif
