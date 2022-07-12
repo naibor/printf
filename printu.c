058747ac078d7149721cc3479b7e5243160bca8c
naibor <liznaibor.en@gmail.com>
2022-07-13 01:01:29 +0300

    create function to print unsigned int

---
+++ printu.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_u - Function
+ *
+ * Description:  Prints an unsigned number
+ *
+ * @u: unsigned integer to be printed
+ *
+ * Return: A count of numbers printed
+ */
+int print_u(va_list u)
+{
+        int div = 1;
+        int count = 0;
+        unsigned int num;
+
+        num = va_arg(u, unsigned int);
+	if (num == 0)
+		_putchar('0');
+	if (num < 1)
+		return (-1);
+        for (; num / div > 9; )
+                div *= 10;
+
+        for (; div != 0; )
+        {
+                count += _putchar('0' + num / div);
+                num %= div;
+                div /= 10;
+        }
+
+        return (count);
+}
+
