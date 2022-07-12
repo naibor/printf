062427ad50db3cfaaeed585d099eaac80ddb473a
JOHNSON.GM <gichukijohnson93@gmail.com>
2022-07-12 23:49:00 +0300

    Update printd.c

--- printd.c
+++ printd.c
@@ -1,29 +1,40 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * print_d - print out decimals no. (base 10)
- *
- * @d: parameter of type va_list, what to print
- *
- * return: 0 (success)
+ * print_d - A function that prints a decimal base 10
+ * @i: base 10 integer to print
+ * Return: number of printed digits
  */
 int print_d(va_list i)
 {
-	int d = va_arg(i, int);
-	int counter = 0;
+	int x[10];
+	int j = 1, m = 1000000000, n, sum = 0, counter = 0;
 
-	if (d == 0)
+	n = va_arg(i, int);
+	if (n < 0)
 	{
-		return(_putchar('0'));
-	}
-	if (d < 0)
-	{
-		d *= -1;
+		n *= -1;
 		_putchar('-');
 		counter++;
-	}	
-	_putchar(d);
-	va_end(i);
-	return (0);
+	}
+	x[0] = n / m;
+
+	for (; j < 10; j++)
+	{
+		m /= 10;
+		x[j] = (n / m) % 10;
+	}
+
+	for (j = 0; j < 10; j++)
+	{
+		sum += x[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + x[j]);
+			counter++;
+		}
+	}
+	return (counter);
 }
 
