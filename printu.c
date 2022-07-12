7b9f95c78cdef51ffb5ebeb2d00bbbc497e3ff2d
naibor <liznaibor.en@gmail.com>
2022-07-13 01:13:42 +0300

    fix betty

--- printu.c
+++ printu.c
@@ -10,25 +10,22 @@
  */
 int print_u(va_list u)
 {
-        int div = 1;
-        int count = 0;
-        unsigned int num;
+	int div = 1, count = 0;
+	unsigned int num;
 
-        num = va_arg(u, unsigned int);
+	num = va_arg(u, unsigned int);
 	if (num == 0)
 		_putchar('0');
 	if (num < 1)
 		return (-1);
-        for (; num / div > 9; )
-                div *= 10;
-
-        for (; div != 0; )
-        {
-                count += _putchar('0' + num / div);
-                num %= div;
-                div /= 10;
-        }
-
-        return (count);
+	for (; num / div > 9; )
+		div *= 10;
+	for (; div != 0; )
+	{
+		count += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+	return (count);
 }
 
