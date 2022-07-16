d0da3c653e7ecd83b0bc6a9b45fb026004f0d9ca
naibor <liznaibor.en@gmail.com>
2022-07-16 23:40:05 +0300

    update function

--- printx.c
+++ printx.c
@@ -1,14 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
  */
 int print_x(va_list x)
 {
-	int i;
+	char *i;
 	unsigned int hx;
 	int counter;
 
 	hx = va_arg(x, unsigned int);
-	counter+= _putchar(comvert(hx, 16));
+	i = convert(hx, 16, 1);
+	printf("USING PRINTF:%s",i);
+	for (counter = 0; i[counter]; counter++)
+	{
+		_putchar(i[counter]);
+	}
 
 	return (counter);
 }
